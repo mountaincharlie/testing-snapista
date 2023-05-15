a0e90c39f652516cc2c8ae3f921bdf76d3f29912
Charlie Harland <ciharland@hotmail.co.uk>
2023-05-15 19:16:09 +0100

    refactor: pass directory paths into functions as variables instead of hardcoded

--- ard.py
+++ ard.py
@@ -14,9 +14,15 @@ TO DO:
 -[DONE] move functions into utils dir
 -[DONE] to set the operators from json, can you loop through the operators given parameters in the json and set them like that?
 -[DONE] retest with the functions in helper file
+-[DONE] create conda environmetn with snap, snapista, gdal and rasterio
+-workout handling using an external dem
+-why does the TF operator cause a section of the fiji scene ocean to be cut out (run a different scene)?
+-why does the uk scene not process correctly?
+-fix the gdal method for converting the final prduct to COG
 -finish creating the json file for catapults ard process (with snap dem) and others from the table
--workout handling using an external dem 
+
 -create dockerfile - with snap and snapista, rememebr to deactivate conda base env before testing
+-try to make a fiji AM scene work with reprojections (output quality will need to be assessed)
 -use try/execpts
 -try to read input from zip file? (will still require checking the manifest.safe exists)
 -try adding option to clear temp dir
@@ -34,31 +40,31 @@ TO DO:
 from utils.helpers import *
 from datetime import datetime
 
-# Set up the logger
+# Set up the logger/
 logger = setup_logger()
 
 
-def prepare_ard(scene, ard_json_path, out_dir):
+def prepare_ard(scene, ard_json_path, input_dir='./input/', static_dir='./static/', temp_dir='./temp/', output_dir='./output/'):
 
-    scene = 'S1A_IW_GRDH_1SDV_20170724T174037_20170724T174100_017616_01D7A7_F0DA'
-    # params =  # json file of parameters
-    # output name =  # call a FUNCTION to abbreviate each operator to build the output name   
+    logger.info(f'processing scene: {scene}')
+    logger.info(f'processing with json: {ard_json_path.split("/")[-1]}')
+    logger.info(f'output directory set to: {output_dir}')  
 
     # checks if a zip file is present in the setup input dir for the scene 
-    find_scene_zip(scene)
+    find_scene_zip(scene, input_dir)
     logger.info('zip file exists, proceeding to find manifest.safe file')
 
     # checks the manifest file is avaliable and makes so if possible
-    find_manifest_file(scene)
+    find_manifest_file(scene, input_dir, temp_dir)
     logger.info('manifest file exists, proceeding to set input and output file names')
 
     # sets name of output file
     output_name = output_file_name(scene, ard_json_path)
-    output_scene = f'{out_dir}{output_name}'
+    output_scene = f'{output_dir}{output_name}'
     logger.info(f'output file name set to: {output_name}')
 
     # defining the input file
-    input_scene = f'./temp/{scene}.SAFE/manifest.safe'  # location of where the manifest.safe file is confirmed to be
+    input_scene = f'{temp_dir}{scene}.SAFE/manifest.safe'  # location of where the manifest.safe file is confirmed to be
     logger.info(f'input file set to: {input_scene}')
 
     # updating the json file with the input file name output name
@@ -80,6 +86,9 @@ def prepare_ard(scene, ard_json_path, out_dir):
     process_finish_time = datetime.now().strftime("%H:%M:%S")  
     logger.info(f'graph processing finished at: {process_finish_time}')
 
+    # converting the output into COG
+
+
 
 def view_xml_graph_from_json(ard_json_path):
 
@@ -99,7 +108,10 @@ first input must be the manifest.SAFE file, then dims
 
 if __name__ == '__main__':
     # view_xml_graph_from_json('./preprocessing_jsons/catapults_ard_snap_dem.json')
-    prepare_ard('S1A_IW_GRDH_1SDV_20170724T174037_20170724T174100_017616_01D7A7_F0DA', './preprocessing_jsons/catapults_ard_snap_dem.json', './output/')  # take a json file name as an argument (e.g. Orb_Cal_ard.json, Orb_Cal_TC_ard.json etc)
+    prepare_ard('S1A_IW_GRDH_1SDV_20170724T174037_20170724T174100_017616_01D7A7_F0DA', './preprocessing_jsons/test_ard.json', './input/', './static/', './temp/', './output/')  # take a json file name as an argument (e.g. Orb_Cal_ard.json, Orb_Cal_TC_ard.json etc)
 
-    # name of scene to process
-    # json of operators and parameters (to be taken from user input in future)
+    # S1A_IW_GRDH_1SDV_20180104T062254_20180104T062319_020001_02211F_A294  - UK scene (mostly land)
+    # S1A_IW_GRDH_1SDV_20230416T180636_20230416T180701_048125_05C928_0F6E  - UK scene (land/sea)
+    # S1A_IW_GRDH_1SDV_20170724T174037_20170724T174100_017616_01D7A7_F0DA  - Fiji scene
+
+
--- utils/helpers.py
+++ utils/helpers.py
@@ -4,11 +4,6 @@ from pathlib import Path
 import zipfile
 import json
 import logging
-# from ard import logger
-
-# from snapista import TargetBand, TargetBandDescriptors
-# Graph.list_operators()
-# Graph.describe_operators() 
 
 
 def setup_logger():
@@ -20,9 +15,9 @@ def setup_logger():
     return logger
 
 
-def find_scene_zip(scene):
+def find_scene_zip(scene, input_dir):
     # checking if there is a zip file present
-    zip_path = Path(f'./input/{scene}.zip')
+    zip_path = Path(f'{input_dir}{scene}.zip')
 
     if not zip_path.is_file():
         logging.info('No zip file found for this scene')  # MAKE INTO LOGS
@@ -33,26 +28,26 @@ def find_scene_zip(scene):
         return
 
 
-def unzip_scene_file(scene, manifest_path):
+def unzip_scene_file(scene, manifest_path, input_dir, temp_dir):
         # unzipping the file into the temp dir if no manifest safe is present yet
     logging.info('not manifest file found in temp directory')
     logging.info(f'unzipping {scene}.zip into temp directory')
 
     # MAKE INTO FUNCTION
-    with zipfile.ZipFile(f'./input/{scene}.zip', 'r') as zip_ref:
-        zip_ref.extractall('./temp/')
+    with zipfile.ZipFile(f'{input_dir}{scene}.zip', 'r') as zip_ref:
+        zip_ref.extractall(f'{temp_dir}')
 
     logging.info(manifest_path.is_file())
     return
 
 
-def find_manifest_file(scene):
+def find_manifest_file(scene, input_dir, temp_dir):
     # checks if there is already access to the manifest.safe file => else need to unzip file
-    manifest_path = Path(f'./temp/{scene}.SAFE/manifest.safe')
+    manifest_path = Path(f'{temp_dir}{scene}.SAFE/manifest.safe')
 
     if not manifest_path.is_file():
         # unzipping the scene file
-        unzip_scene_file(scene, manifest_path)
+        unzip_scene_file(scene, manifest_path, input_dir, temp_dir)
 
         if manifest_path.is_file():
             logging.info(f'{scene}.zip unzipped into temp directory')
@@ -97,7 +92,7 @@ def set_operator_params(current_operator, ard_json_path):
             logging.info(f'setting params for: {current_operator}')
             for param in operators[current_operator]:
                 if param != 'suffix':
-                    logging.info(f'setting the {param} paramater for {current_operator} to: {operators[current_operator][param]}')
+                    # logging.info(f'setting the {param} paramater for {current_operator} to: {operators[current_operator][param]}')
                     setattr(op_object, param, operators[current_operator][param])  # setting the paramater for the operator
 
             return op_name, op_object  # returns the name of the operator and the object itself
